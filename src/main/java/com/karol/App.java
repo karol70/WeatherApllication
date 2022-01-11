055ba3aae80c2f480061c3d4c06cec65d768898d
Karol Węgrzyn <karolwg19@gmail.com>
2022-01-11 15:30:17 +0100

    create main weather application window in fxml

--- src/main/java/com/karol/App.java
+++ src/main/java/com/karol/App.java
@@ -1,13 +1,31 @@
 package com.karol;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/weatherWindow.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+
     }
 }
---
+++ src/main/java/com/karol/controllers/WeatherWindowController.java
@@ -0,0 +1,58 @@
+package com.karol.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class WeatherWindowController {
+
+    @FXML
+    private Button destinationButton;
+
+    @FXML
+    private Label destinationLabel;
+
+    @FXML
+    private TextField destinationTextField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Label fifthDay;
+
+    @FXML
+    private Label fifthDayDest;
+
+    @FXML
+    private Label firstDay;
+
+    @FXML
+    private Label firstDayDest;
+
+    @FXML
+    private Label fourthDay;
+
+    @FXML
+    private Label fourthDayDest;
+
+    @FXML
+    private Label secondDay;
+
+    @FXML
+    private Label secondDayDest;
+
+    @FXML
+    private Label thirdDay;
+
+    @FXML
+    private Label thirdDayDest;
+
+    @FXML
+    private Button yourLocationButton;
+
+    @FXML
+    private Label yourLocationLabel;
+
+}
